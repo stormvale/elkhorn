98242b51e0d6d2ede85f7b6163d6355698a2dca2
Kevin Reid <644802+codeswithfists@users.noreply.github.com>
2025-06-10 15:07:01 -0700

    initial commit

---
+++ test/unit/API.Weather.Tests/WeatherTests.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Weather.Tests;
+
+public class WeatherTests
+{
+    [Fact]
+    public void WeatherForecast_CelciusToFahrenheit_WorksCorrectly()
+    {
+        WeatherForecast forecast = new(DateOnly.FromDateTime(DateTime.Now.AddDays(1)), 5, "Cold");
+        
+        Assert.NotNull(forecast);
+        Assert.Equal(40, forecast.TemperatureF);
+    }
+}
