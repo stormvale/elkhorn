36fa3ff2bfe3abb073229306eed26a31811b2485
Kevin Reid <644802+codeswithfists@users.noreply.github.com>
2025-07-25 21:37:12 -0700

    Users Api workflow (#18)
    * some minor workflow improvements (hopefully)
    
    * added missing service urls to vite env file; added initial empty integration tests project for users api; new users api workflow;

---
+++ test/integration/Users.Api.IntegrationTests/UsersApiTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Users.Api.IntegrationTests;
+
+/// <summary>
+/// Just a placeholder for now.
+/// </summary>
+public class UsersApiTests
+{
+    [Fact]
+    public void GetById_NotAuthenticated_UserDoesNotExist_ProblemDetails404()
+    {
+        Assert.True(true);
+    }
+}
