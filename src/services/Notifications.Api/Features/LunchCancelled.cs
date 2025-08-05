af7e7dbcdc2f6f3c7f7e8833e508bbe6d39abaf8
Kevin Reid <644802+codeswithfists@users.noreply.github.com>
2025-08-04 17:14:16 -0700

    added some UI around creating and cancelling (ie. deleting) lunches; updated notifications to email about cancelled lunches;

---
+++ src/services/Notifications.Api/Features/LunchCancelled.cs
@@ -0,0 +1,25 @@
+﻿using Contracts.Lunches.Messages;
+using Notifications.Api.Services;
+
+namespace Notifications.Api.Features;
+
+public static class LunchScheduled
+{
+    public static void MapLunchScheduled(this WebApplication app)
+    {
+        app.MapPost("/lunch-scheduled", async (
+                LunchScheduledMessage message,
+                ILogger<Program> logger,
+                EmailSender emailSender,
+                CancellationToken ct) =>
+            {
+                logger.LogInformation("New Lunch ({LunchId}) scheduled for {Date}", message.LunchId, message.Date);
+
+                await emailSender.SendEmailForLunchScheduled(message);
+
+                return TypedResults.Ok();
+            })
+        .WithSummary("Lunch Scheduled")
+        .WithTopic("pubsub", "lunch-events"); // subscribes to the topic
+    }
+}
