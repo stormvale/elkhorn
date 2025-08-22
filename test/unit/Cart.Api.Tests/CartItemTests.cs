5c4de3fe24c23641de6eeaec613e581d118ab0b1
Kevin Reid <644802+codeswithfists@users.noreply.github.com>
2025-08-22 16:19:30 -0700

    added missing unit test project and github actions file for cart api

---
+++ test/unit/Cart.Api.Tests/CartItemTests.cs
@@ -0,0 +1,30 @@
+﻿using Cart.Api.Domain;
+using Contracts.Cart.Messages;
+using Contracts.Lunches.Responses;
+using Shouldly;
+
+namespace Cart.Api.Tests;
+
+public class CartItemTests
+{
+    [Fact]
+    public void CartItem_CreateFromMessage_ShouldCreateCorrectly()
+    {
+        var userId = Guid.CreateVersion7();
+        
+        var lunchItemResponse = new LunchItemResponse(
+            Guid.CreateVersion7(),
+            "Test Lunch Item",
+            5.50M,
+            [new LunchItemModifierResponse("Test Modifier", 0.50M)]
+        );
+            
+        var message = AddItemToCartMessage.CreateFrom(userId, lunchItemResponse);
+        var cartItem = CartItem.CreateFrom(message);
+        
+        cartItem.ItemId.ShouldBe(lunchItemResponse.Id);
+        cartItem.ItemName.ShouldBe(lunchItemResponse.Name);
+        cartItem.ItemType.ShouldBe(typeof(LunchItemResponse).FullName);
+        cartItem.Quantity.ShouldBe(1);
+    }
+}
